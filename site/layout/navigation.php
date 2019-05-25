74b1cb23686a27df0e0530e80df449ed953a0368
pheek <git@gress.ly>
2019-05-25 23:01:16 +0200

    CSS für die Navigation verschönert.

--- site/layout/navigation.php
+++ site/layout/navigation.php
@@ -9,12 +9,7 @@ if(strpos($_SERVER['PHP_SELF'], 'ndex.php') > 0) {
 ?>
 
 <div class='tourHeader'>
-	 <table style='width: 100%;'>
-	 <tr style='text-align: center;'>
-	 
-	 <td><a href='<?php echo $ERFINDUNG_PREFIX; ?>abakus&kategorie=1'     >Rechnen</a></td>
-	 <td><a href='<?php echo $ERFINDUNG_PREFIX; ?>amphore&kategorie=2'    >Speichern</a></td>
-	 <td><a href='<?php echo $ERFINDUNG_PREFIX; ?>keilschrift&kategorie=3'>Kommunizieren</a></td>
-	 
-	 </tr></table>
-</div>
+	 <a href='<?php echo $ERFINDUNG_PREFIX; ?>abakus&kategorie=1'     >Rechnen</a>
+<a href='<?php echo $ERFINDUNG_PREFIX; ?>amphore&kategorie=2'    >Speichern</a>
+	<a href='<?php echo $ERFINDUNG_PREFIX; ?>keilschrift&kategorie=3'>Kommunizieren</a>
+	</div>
