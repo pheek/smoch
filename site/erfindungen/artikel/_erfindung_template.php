30890121d954e0ec97ecda286ddb9553556596ab
pheek <git@gress.ly>
2019-05-25 23:06:20 +0200

    Reihenfolge der Titel in den Erfindungen geändert.

--- site/erfindungen/artikel/_erfindung_template.php
+++ site/erfindungen/artikel/_erfindung_template.php
@@ -1,16 +1,15 @@
 <!-- Erfindung -->
-<div class='erfindung'>
-	<h1>Nutzen / Verwendung</h1>
-	<p> .... verwendung comming soon .... </p>
 
-	<h1>Funktionsweise</h1>
-	<p> .... funktionsweise comming soon .... </p>
+<div class='erfindung'>
 
 	<h1>Geschichte</h1>
 	<p> .... geschichte comming soon .... </p>
 
-	<h1>Aufbau</h1>
-	<p> .... aufbau comming soon .... </p>
+	<h1>Verwendung und Nutzen</h1>
+	<p> .... verwendung comming soon .... </p>
+
+	<h1>Funktionsweise und Aufbau</h1>
+	<p> .... funktionsweise comming soon .... </p>
 
 	<h1>Fun-Facts</h1>
 	<p> .... fun-facts comming soon .... </p>
