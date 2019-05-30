0555202116f01ed426d443d7a04fb4a1ca78921f
pheek <git@gress.ly>
2019-05-30 10:09:10 +0200

    Erfindungen (Inhalte)

--- site/erfindungen/artikel/differenzenmaschine.php
+++ site/erfindungen/artikel/differenzenmaschine.php
@@ -3,20 +3,28 @@
 <div class='erfindung'>
 
 	<h1>Geschichte</h1>
-	<p> .... geschichte comming soon .... </p>
-  <p class='zitat text'>ein Zitat</p>
-	<p class='zitat quelle'>Quelle des Zitats</p>
+	<p>Die erste funktionsfähige Differenzenmaschine stammt von <a href='https://de.wikipedia.org/wiki/Charles_Babbage'>Charles Babbage</a>. Die obigen abgebildete Differenzenmaschine ist eine Nachbildung aus den 1970er Jahren und steht im <i>Da Vinci Museum</i> in Mailand.</p>
 	 
 	<h1>Verwendung und Nutzen</h1>
-	<p> .... verwendung comming soon .... </p>
-	<p>Nun <span class='zitat text'>ein Zitat im Fließtext</span> <span class='zitat quelle'>Quelle des Zitates im Fließtext</span>. Weiter im text ...</p>
+	 <p>Die häufigste Nutzung der Differenzmaschinen war die Interpolation von Werten zwischen bekannten Stützstellen. Aber auch z. B. der Sinus konnte damit berechnet werden.</p>
 
 	<h1>Funktionsweise und Aufbau</h1>
-	<p> .... funktionsweise comming soon .... </p>
-
-	<h1>Fun-Facts</h1>
-	<p> .... fun-facts comming soon .... </p>
+     <p>Die Differenzenmaschine rechnet mit Differenzen von Differenzen von Zahlenfolgen. So konnte aus einer Messreihe von Zahlen
+     die nächste "extrapoliert" werden, wenn davon ausgegangen wird, dass die Reihe z. B. eine quadratische Funktion war.</p>
+     <p>Im folgenden Beispiel sind sieben Messwerte gegeben. Was wird wohl der achte sein?</p>
+     <table class='tabelle'>
+     <tr><td>Ordinaler Wert<br />(Messwert Nummer)</td>	<td>Messwert</td>	<td>Differenz</td>	<td>Differenz der Differenz</td></tr>
+<tr><td>1.</td><td>12</td><td></td><td></td></tr>
+     
+     <tr><td>2.</td>	<td>23</td>	<td>11</td><td></td>	</tr>
+<tr><td>3.</td>	<td>40</td>	<td>17</td>	<td>6</td></tr>
+<tr><td>4.</td>	<td>63</td>	<td>23</td>	<td>6</td></tr>
+<tr><td>5.</td>	<td>92</td>	<td>29</td>	<td>6</td></tr>
+<tr><td>6.</td>	<td>127</td>	<td>35</td>	<td>6</td></tr>
+<tr><td>7.</td>	<td>168</td>	<td>41</td>	<td>6</td></tr>
+     <tr><td>8.</td> <td>???</td>  <td>(???)</td><td><b>6</b></td></tr>
+     </table>
 
 	<h1>Quellenangaben</h1>
-	<p> .... Quellenangaben .... </p>
+	 <p>Wikipedia: <a href='https://de.wikipedia.org/wiki/Differenzmaschine'>Differenzenmaschine</a></p>
 </div>
--- site/erfindungen/artikel/zahlenschieber.php
+++ site/erfindungen/artikel/zahlenschieber.php
@@ -3,20 +3,19 @@
 <div class='erfindung'>
 
 	<h1>Geschichte</h1>
-	<p> .... geschichte comming soon .... </p>
-  <p class='zitat text'>ein Zitat</p>
-	<p class='zitat quelle'>Quelle des Zitats</p>
+	<p>Zahlenschieber nach verschiedenen Patenten tauchten im 18. Jahrhundert auf. Die Seite <a href='https://history-computer.com/MechanicalCalculators/Pioneers/Perrault.html'>history-computer.com</a> zeigt eine Erfindung von 1699.</p>
 	 
 	<h1>Verwendung und Nutzen</h1>
-	<p> .... verwendung comming soon .... </p>
-	<p>Nun <span class='zitat text'>ein Zitat im Fließtext</span> <span class='zitat quelle'>Quelle des Zitates im Fließtext</span>. Weiter im text ...</p>
+	<p>Parallel angeordnete Ziffernstreifen werden zur Addition ähnlich wie bei einem Abakus verschoben. Dazu können vielstellige Zahlen addiert und subtrahiert werden.</p>
 
 	<h1>Funktionsweise und Aufbau</h1>
-	<p> .... funktionsweise comming soon .... </p>
+	 <p>Die Ziffernstäbchen enthalten für den Anwender unsichtbare Zähnchen, welche beim Zehnerübertrag eine Stelle im nächsthöheren Stellenwert verschieben.</p>
 
 	<h1>Fun-Facts</h1>
-	<p> .... fun-facts comming soon .... </p>
+	 <p>Für den Übertrag werden Zahlen nach oben verschoben. Es ist sogar möglich, über mehrer Stellen zu Addieren; zum Beispiel 999 + 2 ergibt 1001; dazu ist jedoch händischer Eingriff nötig, der aber beim ADDIMULT SUPRA schön mit einem roten Pfleil in Erinnerung gerufen wird.</p>
+				      <p>Ein "RESET"-Bügel setzt alle Werte wieder auf Null.</p>
 
-	<h1>Quellenangaben</h1>
-	<p> .... Quellenangaben .... </p>
+				      
+	<h1>DER Weblink</h1>
+	<p><a href='https://history-computer.com'>history-computer.com</a></p>
 </div>
