8ef92638e9a4f7de8384347b823536bf17defd90
pheek <git@gress.ly>
2019-06-04 07:34:13 +0200

    Computer als Erfindung hinzugefügt

--- site/erfindungen/artikel/computer.php
+++ site/erfindungen/artikel/computer.php
@@ -1,7 +1,22 @@
 <!-- Erfindung -->
 
 <div class='erfindung'>
-	 <h1>Computer: Im Aufbau</h1>
-	<p>Die Seite zu dieser Erfindung ist im Aufbau.</p>
-	<img src='../layout/img/uc.png' />
+
+		<h1>Geschichte</h1>
+	 <p>Die Geschichte des Computers reicht natürlich mit allen Vorbereitungen weiter zurück als der Bau des Zuse Z3 von Konrad Zuse im Jahre 1937.
+		 Vorbereitungen trafen bereits Allan Turing mit seiner Turingmaschine (1936). Weitere Verbesserungen kamen später dazu, wie z. B. die "Von Neumann-Architektur" von John von Neumann (1947).</p>
+  <p class='zitat text'>ein Zitat</p>
+	<p class='zitat quelle'>Quelle des Zitats</p>
+	 
+	<h1>Verwendung und Nutzen</h1>
+	 <p>Computer werden für verschiedenste Berechnungen (rechnen engl. 'to compute'), aber auch zum Internetsurfen, zum Spielen oder einfach (wie Linus Torwalds es nennt) "just for fun" verwendet. Wie der Autor dieser Seiten die Computer vorwiegend zum Programmieren verwendet (und nicht zur Anwendung).
+
+
+	<h1>Funktionsweise und Aufbau</h1>
+	<p>Die Von Neumann Architektur zeigt den Aufbau und die Funktionsweise wohl am besten: Link <a href='https://de.wikipedia.org/wiki/Von-Neumann-Architektur'>Von Neumann Architektur</a>.</p>
+
+	<h1>Fun-Facts</h1>
+	 <p>Als Computer für Laien erschwinglich wurden, hat sich der Autor 1987 einen <i>Comodore Amiga 1000</i> gekauft. Da er in der Gemeinde (neben der Gemeindsverwaltung) einer der ersten Computerbesitzer war, fragte seine Mutter erstaunt: «Was willst Du mit so einem Riesenmöbel, wozu ist das überhaupt gut?» (Heute ist ebendiese Mutterr im Besitze eines <i>Apple Powerbooks</i> mit über tausendfacher Leistung des damaligen <i>Amiga 1000</i>). Die ersten Computerspiele, welche er schrieb sind <a href='http://zarkonnen.github.io/SwissGamesShowcase/pamehta.html'>Pamehta</a> und <a href='https://www.gameculture.ch/2011/01/zuri-revisited-oder-schweizer-game-design-im-20-jh/'>Züri</a>.</p>
+
+		               
 </div>
