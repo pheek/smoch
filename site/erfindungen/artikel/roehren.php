04a98191841900bdcc13605e45bb64ea46f90fe5
pheek <git@gress.ly>
2019-06-26 14:10:31 +0200

    Röhrenrechner Text

--- site/erfindungen/artikel/roehren.php
+++ site/erfindungen/artikel/roehren.php
@@ -1,7 +1,26 @@
 <!-- Erfindung -->
 
 <div class='erfindung'>
-	<h1>Röhrenrechner: Im Aufbau</h1>
-	<p>Die Seite zu dieser Erfindung ist im Aufbau.</p>
-	<img src='../layout/img/uc.png' />
+	<h1>Geschichte</h1>
+ <p>Röhrencomputer sind das Bindeglied zwischen Relais-Rechnern und
+		Rechnern, welche auf Halbleiterbasis funktionieren.
+	</p> 
+
+	<h1>Verwendung und Nutzen</h1>
+	<p>Röhrenrechner haben theoretisch keinen neuen Nutzen gegenüber den
+	Relais-Rechnern.</p>
+
+	<h1>Funktionsweise und Aufbau</h1>
+	<p> Der wichtigste Vorteil von Röhren gegenüber Relais ist jedoch, dass
+	Röhrenrechner keine mechanischen Teile benötigen, um eine Berechnung
+	(AND, NOT, OR, XOR, ...) durchzuführen. </p>
+
+
+		<h1>Fun-Facts</h1>
+		<p>Die abgebildete Röhre stammt aus einem Röhrenverstärker und hat
+		direkt nichts mit einem Röhrencomputer zu tun. Einzig die
+		Tatsache, das die Röhre, analog zum Transistor, wie ein Verstärker
+		funktioniert, der auch als Schlater verwendet werden kann.</p>
+
+
 </div>
