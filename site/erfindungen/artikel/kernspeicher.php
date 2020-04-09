72411ba70738ba9d7fac9cf83520b0bbf507b96f
phi <phi@freimann.eu>
2020-04-09 15:29:19 +0200

    Typo

--- site/erfindungen/artikel/kernspeicher.php
+++ site/erfindungen/artikel/kernspeicher.php
@@ -4,13 +4,13 @@
 
 	<h1>Geschichte</h1>
        <p>Kernspeicher stehen geschichtlich zwischen den <a href='https://de.wikipedia.org/wiki/Verz%C3%B6gerungsleitung#Geschichte'>Laufzeitspeichern</a> und dem modernen RAM (Random Access Memory)</p>
-                                                                                                                                                                                    <p>Kernspeicher wurden in den frühen 50-er Jahren des 20. Jahrhunderts entwickelt und waren so bis 1980 im Einsatz.</p>                                                                                                                                                                                    
-	<p class='zitat quelle'>Wikipedia 2020</p>
+                                                                                                                                                              <p>Kernspeicher wurden in den frühen 50-er Jahren des 20. Jahrhunderts entwickelt und waren so bis 1980 im Einsatz.</p>
+
+<p class='zitat quelle'>Wikipedia 2020</p>
 	 
 	<h1>Verwendung und Nutzen</h1>
-                         <p>Da ein Kernspeicher die Informationen auch ohne permanenten Strom behalten kann, wird er als nicth-flüchtig bezeichnet. Nicht-flüchtige Speicher dienen dazu, eine Information (Daten / Programme) auch über den Neustart einer Rechenanlage aufgezeichnet zu halten.</p>
-                                                                                                                                                                                                                                                                                                
-                                                                                                                                    
+                         <p>Da ein Kernspeicher die Informationen auch ohne permanenten Strom behalten kann, wird er als nicht-flüchtig bezeichnet. Nicht-flüchtige Speicher dienen dazu, eine Information (Daten / Programme) auch über den Neustart einer Rechenanlage aufgezeichnet zu halten.</p>
+
 	<h1>Funktionsweise und Aufbau</h1>
                              <p>Kleine Ferritringe können durch einen Stromfluss magnetisiert werden. Diese Magnetisierung verändert einen weiteren Stromfluss und damit kann ausgesagt werden, ob der ring "ein"- oder "aus"geschaltet ist.</p>
 
