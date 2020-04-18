bc55b56a969f9ff8c993e83ea71e968919bb7c32
phi <phi@freimann.eu>
2020-04-18 21:37:24 +0200

    Neue Texte + Skript
    Neue Texte bei volladierer und bei bit_transistor.
    
    Zudem habe ich das Skript "Grundlagen der Digitaltechnik" hochgeladen.

--- site/erfindungen/artikel/bit_transistor.php
+++ site/erfindungen/artikel/bit_transistor.php
@@ -1,7 +1,51 @@
 <!-- Erfindung -->
 
 <div class='erfindung'>
-	<h1>Bit realisiert mit Tranistoren: Im Aufbau</h1>
-	<p>Die Seite zu dieser Erfindung ist im Aufbau.</p>
-	<img src='../layout/img/uc.png' />
+
+	<h1>Geschichte</h1>
+	 <p>Wann das erste Bit mit Transistoren gebaut wurde, ist mir nicht bekannt. Wahrscheinlich war das Bit bereits mit Röhren realisiert worden, denn der Schaltungsaufbau ist im Grundprinzip obiger Schaltung identisch. (Hinweise gerne an phi@smoch.ch)</p>
+
+	 
+	<h1>Verwendung und Nutzen</h1>
+	<p>Ein Bit ist die kleinste digitale Informationseinheit. Damit
+	kann z. B. eine Null von einer Eins unterschieden werden, es kann
+	"true" im Gegensatz zu "false" modelliert werden, es kann das
+		Ergebnis einer NOT-Operation auch gespeichert werden.</p>
+
+	<p>Obschon ein Bit die kleinste Informationseinheit ist, so kann
+	damit ein ganzer Programmablauf durch eine einzige Selektion
+	verändert werden; Beispiel: Ein Web-Shop kann in einem einzigen Bit
+	speichern, ob der aktuelle Benutzer eingeloggt ist oder nicht und
+	somit komplett andere Inhalte anzeigen.</p>
+	 
+	<h1>Funktionsweise und Aufbau</h1>
+	 <p>Die beiden Taster (unten in den Bildern) sind in der Praxis
+	 natürlich durch externe Ströme zu realisieren. Diese <i>externen
+	 Ströme</i> können durch Spannungen an der Basis der beiden
+	 Transistoren oder durch weitere Transistoren statt den Tastern
+		 umgesetzt werden.
+	 </p>
+	<p>Hier dienen die beiden Taster jedoch der
+	 Veranschaulichung. Wird ein Taster gedrückt, so fließt dort der
+	 Strom ab und der Basisstrom am Transistor wird unterbrochen. Somit
+	 fließt der Strom durch den anderen Transistor und behält seinen
+		Zustand solange, bis der andere Taster gedrückt wird.
+	</p>
+
+	<p>Die obige Schaltung wurde mit ca. 8.5 Volt getestet. Beim
+	gesperrten rechten Transistor liefert der Output ca. 7.5 Volt, was
+	noch gut reicht, um weitere Transistoren anzusteuern.</p>
+
+	
+
+	<h1>Fun-Facts</h1>
+	 <p>Die obige Schaltung wurde einen <a href='https://www.smoch.ch/skript/Grundlagen_Digitaltechnik_2020_04_18_V1.19.pdf'>Kurs</a>
+	 entwickelt. Die Widerstände sind nicht diejenigen, welche meistens
+	 zusammen mit dem BC548C verwendet werden. Die Widerstände wurden so
+		 gewählt, dass die Farben gut unterschieden werden können.</p>
+	 
+	 <h1>Quellenangaben</h1>
+	<p>Dieses Exponat wurde vom Kurator persönlich gebaut. Der
+	Schaltplan stimmt mit der Schaltung überein.</p>
+
 </div>
--- site/erfindungen/artikel/volladdierer.php
+++ site/erfindungen/artikel/volladdierer.php
@@ -1,7 +1,55 @@
 <!-- Erfindung -->
 
+
 <div class='erfindung'>
-	 <h1>Volladdierer: Im Aufbau</h1>
-	<p>Die Seite zu dieser Erfindung ist im Aufbau.</p>
-	<img src='../layout/img/uc.png' />
+
+	<h1>Geschichte</h1>
+	<p>Wann der erste Volladdierer gebaut wurde, ist mir nicht
+	bekannt. Definitiv musste bereits Zuse addieren können. Das addieren
+	mit Relais geht vom Prinzip jedoch genau gleich, wie mit Röhren oder
+	Transistoren.</p>
+	 
+	<h1>Verwendung und Nutzen</h1>
+	<p>Im Wesentlichen können mit einem Mehr-Bit-Addierer zwei
+		Binärzahlen zusammengezählt werden. Dank der speziellen Kodierung
+		im 2er-Komplement können damit auch Zahlen subtrahiert werden.</p>
+
+		<h1>Funktionsweise und Aufbau</h1>
+
+	<p>Um
+	zwei Ziffern zusammenzuzählen reicht ein sog. <b>Halbaddierer</b> (HA). Dieser
+	liefert von der Summe von zwei Ziffern die Summenziffer plus den
+		Übertrag.
+		Im Binärsystem ist der Übertrag nichts anderes als die
+		"AND"-Verknüpfung der beiden Ziffern während die Summe das "XOR"
+		darstellt.</p>
+<p>
+Ein <b>Volladdierer</b> (VA) addiert nicht nur zwei, sondern drei
+Stellen. Dies ist ab der 2. Ziffer nötig, denn jede vorangehende
+Ziffer könnte ja einen Übertrag liefern.</p>
+
+<p>Der abgebildete <b>Vier-Bit-Addierer</b> besteht aus einem
+	Halbaddierer (für die Einerziffer) und aus drei Volladdierern (für
+	die Zweier-, die Vierer- und die Achterziffer). A1 bis A4
+	bezeichnen die erste Zahl während B1 bis B4 die zweite Zahl
+	bezeichnen. Die Zahlen sind binär codiert; so bezeichnet A1 die
+	Einerziffer, A2 die Zweierziffer, A3 die Viererziffer und A4 die
+	Achterziffer (Analog B1 bis B4). C1 bis C5 werden zur Summe der
+	beiden Zahlen A und B.</p>
+
+	<h1>Fun-Facts</h1>
+	<p>Der 4-Bit-Addierer mit Relais nachgebaut ist eine schöne
+	Demonstration, wie ein Computer im innersten zwei Zahlen miteinander
+	addieren kann. Die Umsetzung mit Transistoren (Bild) ist durchaus
+	schwieriger, Fehleranfälliger und es ist auch durchaus schwieriger
+	Fehler zu finden; mit anderen Worten: Der 4-Bit-Addierer mit
+	Transistoren ist wohl unser Meisterwerk, dennoch niemals so spannend,
+	wie mit Relais nachgebaut. Bei den Relais hört man die Addition.</p>
+
+	<h1>Quellenangaben</h1>
+	<p>Die Webseite <a href='https://www.waitingforfriday.com'>waiting for friday</a> hat mich auf die Idee gebracht,
+	einen 4-Bit-Addierer auch mit Transistoren nachzubauen. Ich habe
+	jedoch andere Werte in den Widerständen genommen, sodass sich die
+	Widerstände farblich besser voneinander abheben. Zudem haben wir
+	das XOR jeweils mit lediglich 5 Transistoren gebaut.</p>
 </div>
