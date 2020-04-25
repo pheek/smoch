2fdf81fafab5332aba14c99965e2d818b0429de5
phi <phi@freimann.eu>
2020-04-25 23:15:56 +0200

    Zwei neue bzw verbesserte Artikel

--- site/erfindungen/artikel/kernspeicher.php
+++ site/erfindungen/artikel/kernspeicher.php
@@ -3,21 +3,24 @@
 <div class='erfindung'>
 
 	<h1>Geschichte</h1>
-       <p>Kernspeicher stehen geschichtlich zwischen den <a href='https://de.wikipedia.org/wiki/Verz%C3%B6gerungsleitung#Geschichte'>Laufzeitspeichern</a> und dem modernen RAM (Random Access Memory)</p>
-                                                                                                                                                              <p>Kernspeicher wurden in den frühen 50-er Jahren des 20. Jahrhunderts entwickelt und waren so bis 1980 im Einsatz.</p>
-
-<p class='zitat quelle'>Wikipedia 2020</p>
+		<p>Kernspeicher (auch Fädelspeicher) stehen geschichtlich zwischen den <a href='https://de.wikipedia.org/wiki/Verz%C3%B6gerungsleitung#Geschichte'>Laufzeitspeichern</a> und dem modernen RAM (Random Access Memory)</p>
+		<p>Kernspeicher wurden in den frühen 50-er Jahren des 20. Jahrhunderts entwickelt und waren so bis 1980 im Einsatz.</p>
+		<p class='zitat quelle'>Wikipedia 2020</p>
 	 
 	<h1>Verwendung und Nutzen</h1>
-                         <p>Da ein Kernspeicher die Informationen auch ohne permanenten Strom behalten kann, wird er als nicht-flüchtig bezeichnet. Nicht-flüchtige Speicher dienen dazu, eine Information (Daten / Programme) auch über den Neustart einer Rechenanlage aufgezeichnet zu halten.</p>
+		<p>Da ein Kernspeicher die Informationen auch ohne permanenten Strom behalten kann, wird er als nicht-flüchtig bezeichnet. Nicht-flüchtige
+		   Speicher dienen dazu, eine Information (Daten / Programme) auch über den Neustart einer Rechenanlage aufgezeichnet zu halten.</p>
 
 	<h1>Funktionsweise und Aufbau</h1>
-                             <p>Kleine Ferritringe können durch einen Stromfluss magnetisiert werden. Diese Magnetisierung verändert einen weiteren Stromfluss und damit kann ausgesagt werden, ob der ring "ein"- oder "aus"geschaltet ist.</p>
+		<p>Kleine Ferritringe können durch einen Stromfluss magnetisiert werden. Diese Magnetisierung verändert einen weiteren Stromfluss
+		   und damit kann ausgesagt werden, ob der ring "ein"- oder "aus"geschaltet ist.</p>
 
 	<h1>Fun-Facts</h1>
-	<p>Auf die Kernspeicher wurde ich erst aufmerksam, da ich im Keller meines ehemaligen Arbetgebers im Nachlass eines Mitarbeiters zwei Kernspeicher gefunden hatte. Der im SMOCH ausgestellte Kernspeicher ist hingegen auf ricardo.ch gekauft worden.</p>
+		<p>Auf die Kernspeicher wurde ich erst aufmerksam, da ich im Keller meines ehemaligen Arbeitgebers im Nachlass eines Mitarbeiters zwei
+ 		   Kernspeicher gefunden hatte. Der im SMOCH ausgestellte Kernspeicher ist hingegen auf ricardo.ch gekauft worden.</p>
+		<p>Der Begriff «core dump» (Speicherauszug) geht noch auf die alten "cores" (Kerne) zurück.</p>
 
 	<h1>Quellenangaben</h1>
-	<p>Wikipedia 2020</p>
+		<p>Wikipedia 2020</p>
 
 </div>
--- site/erfindungen/artikel/transistor.php
+++ site/erfindungen/artikel/transistor.php
@@ -1,7 +1,25 @@
 <!-- Erfindung -->
 
+
 <div class='erfindung'>
-	<h1>Im Aufbau</h1>
-	<p>Die Seite zu dieser Erfindung ist im Aufbau.</p>
-	<img src='../layout/img/uc.png' />
+
+	<h1>Geschichte</h1>
+	<p>Julius E. Lilienfeld beschreibt 1925 die Funktionsweise eines Feldeffekt-Transistors. 1947 wurde der Bipolartransistor von Walter Brattain aufgrund von Erkenntnissen von John Bardeen entwickelt.</p>
+	 
+	<h1>Verwendung und Nutzen</h1>
+                         <p>Transistoren wirken als Verstärker von Strom. Eine kleine Eingangsspannung (typ. 0.7 Volt) an der Basis «öffnen» den Transistor und lassen den Strom quasi Widerstandsfrei von Emitter zum Kollektor fließen. Genau genommen handelt es sich um eine Strom-Verstärkung, die aber so groß ist, dass sie als elektronischer Schalter verwendet werden kann. 
+
+	<h1>Funktionsweise und Aufbau</h1>
+                             <p>Werden Halbleiter (typischerweise Silizium oder Germanium) mit Fremdatomen versetzt, welche die selbe Gitterstruktur, jedoch andere Valenzelektronenzahlen aufweisen versetzt, so entstehen aus den Halbleitern Leiter. Der Strom kann darin quasi Verlustfrei fließen. Wo diese positiv bzw. Negativ dottierten (dottiert, weil nur wenige Fremdatome reichen) Schichten aufeinander treffen, entsteht je nach Stromrichtung eine Sperrschicht bzw. eine stromdurchlässige Schicht. Drei solche Schichten werden übereinander gebracht und als pnp bzw. npn-Transistor bezeichnet (Positiv-Negativ-Positiv bzw. Negativ-Positiv-Negativ).</p>
+
+	<h1>Fun-Facts</h1>
+	<p>Wie mit Transistoren ein Bit gespeichert werden kann sehen Sie <a href='https://smoch.ch/erfindung.php/bit_transistor'>hier</a>.</p>
+  <p>Wie mit Transistoren eine NOT-Schaltung gebaut werden kann, erfahren Sie <a href='https://smoch.ch/erfindung.php/not_transistor'>hier</a>.</p>
+
+	<h1>Quellenangaben</h1>
+	<p><a href='https://de.wikipedia.org/wiki/Transistor'>Wikipedia</a></p>
 </div>
+
+
+
+
