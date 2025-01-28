b971c4db2357de13010160c9a920f82aedb09cec
phi <phi@freimann.eu>
2025-01-28 21:21:35 +0100

    Neue Info zur HD

--- site/erfindungen/artikel/harddisk.php
+++ site/erfindungen/artikel/harddisk.php
@@ -1,7 +1,21 @@
 <!-- Erfindung -->
 
 <div class='erfindung'>
-	 <h1>Harddisk: Im Aufbau</h1>
-	<p>Die Seite zu dieser Erfindung ist im Aufbau.</p>
-	<img src='../layout/img/uc.png' />
+
+<h1>Geschichte</h1>
+       <p>Die Harddisk wurde <a href='https://en.wikipedia.org/wiki/Disk_storage'>1956</a> von IBM (international business machines) eingeführt</p>
+       
+	 
+	<h1>Verwendung und Nutzen</h1>
+                    <p>Durch die drehenden Scheiben, wo die Daten auf konzentrischen Spuren abgelegt sind, kann viel rascher an jede Stelle innerhalb der Dateien zugegriffen werden, als noch beim <a href='/erfindung.php/magnetband'>Magnetband</a>. Wie beim Magnetband wird die Oberfläche magnetisiert und mit Lese- bzw. Schreibkopf manipuliert.</p>
+
+	<h1>Funktionsweise und Aufbau</h1>
+ <p>Im Gegensatz zum Magnetband ist die Scheibe hart, daher Harddisk. In einer Trommel wurden mehrere (anfänglich üblicherweise 6 bis 10, doch die Zahl kann variieren) Disks aufeinander gestapelt und die Lese-/Schreibköpfe waren alle jeweils auf dem selbem Zylinder positioniert. Durch sehr schnelles Drehen der Scheiben, konnten alle Sektoren der selben Zylinder sehr rasch gelesen bzw. beschrieben werden.</p>
+
+	<h1>Trivia</h1>
+       <p>Die ausgestellte Harddisk der IBM stammt aus einer IBM 360 der ehemaligen «Credit Suisse». Üblich war es, die Platten zu Spindeln zusammenzufassen und ich (der Kurator) muss davon ausgehen, dass es sich um eine 14-inch (356 mm)<a href='https://en.wikipedia.org/wiki/Disk_pack'>IBM 1316-Disk-Pack</a>-Spindel handelt (leider habe ich hierzu keine weitere Information bei der Übergabe erhalten). Kapazität: ca 1.5 bis 20 Megabyte pro Disk.</p>
+
+	<h1>Quellenangaben</h1>
+       <p><a href='https://en.wikipedia.org/wiki/Disk_storage'>Wikipedia</a> (2025)</a>
+
 </div>
